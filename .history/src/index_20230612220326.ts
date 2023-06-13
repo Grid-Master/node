a5af7efc585d6e2a9731743c6f3fb86574e1509a
Grid-Master <you@example.com>
2023-06-13 23:56:01 +0300

    first commit

---
+++ .history/src/index_20230612220326.ts
@@ -0,0 +1,11 @@
+import express, { Request, requestesponse } from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Baga!111');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
